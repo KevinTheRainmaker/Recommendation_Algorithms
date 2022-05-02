1cb7bdb955ebaac083debe59b70c99b9ecc47bc1
Kangbeen Ko <sweetrainforyou@gmail.com>
2022-05-02 15:59:43 +0900

    feat(executeQuery()): executing query and return db as dataframe

---
+++ lib.py
@@ -0,0 +1,31 @@
+import mysql.connector
+import pandas as pd
+import yaml
+import sys
+
+def executeQuery(query):
+    arr = []
+    try:
+        conf = yaml.safe_load(open('./config.yml'))
+        mydb = mysql.connector.connect(
+            host=conf['config']['host'],
+            user=conf['config']['user'],
+            passwd=conf['config']['passwd'],
+            database=conf['config']['database'],
+            port = conf['config']['port']
+        )
+    except mysql.connector.Error as e:
+        raise ValueError('Error %d: %s' % (e.args[0], e.args[1]))
+        
+    cursor = mydb.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for result in results:
+        arr.append(result)
+        
+    field_names = [i[0] for i in cursor.description]
+    
+    df = pd.DataFrame(arr, columns=field_names)
+    mydb.close()
+    
+    return df
